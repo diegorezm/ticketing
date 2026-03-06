89f7f2bbfb921ea4b8d4cc6a39ba3bacdf52d2c2
diegorezm <dmourarezende@gmail.com>
2026-03-06 11:02:30 -0300

    footer does not need to be fixed like that

--- src/features/landing_page/ui/components/footer.tsx
+++ src/features/landing_page/ui/components/footer.tsx
@@ -2,7 +2,7 @@ import { Ticket } from 'lucide-react'
 
 export function Footer() {
   return (
-    <footer className="fixed inset-x-0 bottom-0 max-w-6xl mx-auto border-t border-border px-10 py-5 flex items-center justify-between gap-2">
+    <footer className="bottom-0 max-w-6xl mx-auto border-t border-border px-10 py-5 flex items-center justify-between gap-2">
       <span className="text-primary font-semibold text-sm flex items-center gap-2">
         <Ticket size={15} /> Ticketing
       </span>
