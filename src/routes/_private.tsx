f31c75b08b5f730a00fd36767740717448252e03
diegorezm <dmourarezende@gmail.com>
2026-03-06 11:02:09 -0300

    better redirect

--- src/routes/_private.tsx
+++ src/routes/_private.tsx
@@ -2,11 +2,12 @@ import { getSessionAction } from '#/features/auth/actions/get-session-action'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_private')({
-  beforeLoad: async ({}) => {
+  beforeLoad: async ({ location }) => {
     const session = await getSessionAction()
     if (!session) {
       throw redirect({
         to: '/login',
+        search: { redirect: location.href },
       })
     }
   },
