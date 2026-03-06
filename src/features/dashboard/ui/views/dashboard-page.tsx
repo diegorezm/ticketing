c16054dc15d2603654b4d6e6267bd89937d66ab9
diegorezm <dmourarezende@gmail.com>
2026-03-05 22:19:24 -0300

    the dashboard-page component will act as the layout for now

--- src/features/dashboard/ui/views/dashboard-page.tsx
+++ src/features/dashboard/ui/views/dashboard-page.tsx
@@ -1,3 +1,18 @@
+import { SidebarProvider } from '#/components/ui/sidebar'
+import { Outlet } from '@tanstack/react-router'
+import { DashboardSidebar } from '../components/dashboard-sidebar'
+import { DashboardNavbar } from '../components/dashboard-navbar'
+
 export function DashboardPage() {
-  return <div>dash</div>
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <div className="flex flex-col w-full min-h-screen">
+        <DashboardNavbar />
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
+    </SidebarProvider>
+  )
 }
