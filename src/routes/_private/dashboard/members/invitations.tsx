e0eaddce3a65f4143b31d19a7242600e7ebf322f
diegorezm <dmourarezende@gmail.com>
2026-03-13 23:05:50 -0300

    add safeguard

--- src/routes/_private/dashboard/members/invitations.tsx
+++ src/routes/_private/dashboard/members/invitations.tsx
@@ -1,8 +1,17 @@
+import { hasPermissionFn } from '#/features/auth/api/has-permission'
 import { InvitationsPage } from '#/features/invitations/ui/views/invitations-page'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_private/dashboard/members/invitations')(
   {
     component: InvitationsPage,
+    beforeLoad: async () => {
+      const result = await hasPermissionFn({
+        data: { resource: 'organizations', actions: ['invite'] },
+      })
+      if (!result.success) {
+        throw redirect({ to: '/dashboard' })
+      }
+    },
   },
 )
